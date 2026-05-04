0e8870f4487f4bad4999508654f083e8ca70db09
bipinhoro <bipin6768@gmail.com>
2026-05-04 14:09:53 +0530

    Delete Bips.py

--- Bips.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv('customer_shopping_behavior.csv')
-
-# Wrap head() in a print function
-print(df.head())
-
-# This line is already correct and should be appearing
-print(df.columns)
